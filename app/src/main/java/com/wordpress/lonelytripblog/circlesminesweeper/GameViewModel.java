015a7f90b0a8f9b3b2b8b7da0e8c0b20371186bd
pavel <pavelsuvit@gmail.com>
2018-10-24 00:49:23 +0300

    Add tests to verify action down effect, implement action down in GameViewModel

--- app/src/main/java/com/wordpress/lonelytripblog/circlesminesweeper/GameViewModel.java
+++ app/src/main/java/com/wordpress/lonelytripblog/circlesminesweeper/GameViewModel.java
@@ -3,6 +3,8 @@ package com.wordpress.lonelytripblog.circlesminesweeper;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.Nullable;
+import android.support.v4.util.Pair;
 
 import com.wordpress.lonelytripblog.circlesminesweeper.data.Circle;
 import com.wordpress.lonelytripblog.circlesminesweeper.data.CirclesGenerator;
@@ -17,6 +19,7 @@ public class GameViewModel extends ViewModel {
     private GameLevel level;
     private int width;
     private int height;
+    private MutableLiveData<Circle[][]> circlesLiveData = new MutableLiveData<>();
 
     public GameViewModel(final CirclesGenerator circlesGenerator) {
         this.circlesGenerator = circlesGenerator;
@@ -28,7 +31,7 @@ public class GameViewModel extends ViewModel {
     // so array with size 3X4 returned even in portrait orientation,
     // while technically it should be 4x3
     public LiveData<Circle[][]> getCircles() {
-        return new MutableLiveData<>();
+        return circlesLiveData;
     }
 
     public void setLevel(final GameLevel level) {
@@ -43,17 +46,51 @@ public class GameViewModel extends ViewModel {
     public void startGame() {
         throwExceptionIfSizeNotSet();
         Circle[][] circles = getCirclesForLevel();
+        circlesLiveData.setValue(circles);
     }
 
     private Circle[][] getCirclesForLevel() {
-        level.generateCircles(circlesGenerator, width, height);
-        return null;
+        return level.generateCircles(circlesGenerator, width, height);
     }
 
     private void throwExceptionIfSizeNotSet() {
-        if (width == 0 || height == 0) {
+        if (width <= 0 || height <= 0) {
             throw new RuntimeException("Specify width and height for game window");
         }
+    }
+
+    public void actionDown(final int x, final int y) {
+        Pair<Integer, Integer> rowAndColumn = findRowAndColumnForPosition(x, y);
+        if (rowAndColumn == null) return;
+        moveCircleAndUpdateLiveData(x, y, rowAndColumn.first, rowAndColumn.second);
+    }
+
+    @Nullable
+    private Pair<Integer, Integer> findRowAndColumnForPosition(final int x, final int y) {
+        Circle[][] currentCircles = circlesLiveData.getValue();
+        if (currentCircles == null) return null;
+        // TODO optimize algorithm from O(n^2) to O(1)
+        for (int i = 0; i < currentCircles.length; i++) {
+            for (int j = 0; j < currentCircles[0].length; j++) {
+                Circle circle = currentCircles[i][j];
+                if (circle.getX() + circle.getRadius() > x && circle.getX() - circle.getRadius() < x
+                        && circle.getY() + circle.getRadius() > y && circle.getY() - circle.getRadius() < y) {
+                    return new Pair<>(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    private void moveCircleAndUpdateLiveData(final int x, final int y,
+                                             final int row, final int column) {
+        Circle[][] currentCircles = circlesLiveData.getValue();
+        Circle ourCircle = currentCircles[row][column];
+        int oldRadius = ourCircle.getRadius();
+        ourCircle.setX(x);
+        ourCircle.setY(y);
+        ourCircle.setRadius(oldRadius - (oldRadius / 8));
+        circlesLiveData.setValue(currentCircles);
     }
 
 }
--- app/src/test/java/com/wordpress/lonelytripblog/circlesminesweeper/GameViewModelTests.java
+++ app/src/test/java/com/wordpress/lonelytripblog/circlesminesweeper/GameViewModelTests.java
@@ -19,8 +19,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GameViewModelTests {
 
@@ -28,12 +33,12 @@ public class GameViewModelTests {
     public InstantTaskExecutorRule rule = new InstantTaskExecutorRule();
     private GameViewModel viewModel;
     private CirclesGenerator circlesGenerator = mock(CirclesGenerator.class);
+    private Observer<Circle[][]> circleObserver;
 
     @Before
     public void setUp() {
         viewModel = new GameViewModel(circlesGenerator);
-        @SuppressWarnings("unchecked")
-        Observer<Circle[][]> circleObserver = (Observer<Circle[][]>) mock(Observer.class);
+        circleObserver = (Observer<Circle[][]>) mock(Observer.class);
         viewModel.getCircles().observeForever(circleObserver);
     }
 
@@ -105,6 +110,52 @@ public class GameViewModelTests {
         viewModel.startGame();
 
         verify(circlesGenerator).generateCirclesForField4X6(100, 100, 4);
+    }
+
+    @Test
+    public void initialValueOfLiveDataIsCorrect() {
+        // blue_ball green_ball
+        // green_ball blue_ball
+        Circle[][] circles = new Circle[2][2];
+        circles[0][0] = new Circle(50, 50, 50, R.color.blue_ball_center, false);
+        circles[0][1] = new Circle(150, 50, 50, R.color.green_ball_center, false);
+        circles[1][0] = new Circle(50, 150, 50, R.color.green_ball_center, false);
+        circles[1][1] = new Circle(150, 150, 50, R.color.blue_ball_center, false);
+        // This method is generic and not restricting the size of returned circles,
+        // so don't be fool by 3X4 at the end, it's just for readability of CirclesGenerator
+        when(circlesGenerator.generateCirclesForField3X4(anyInt(), anyInt(), anyInt()))
+                .thenReturn(circles);
+
+        GameLevel level = new FirstLevel();
+        viewModel.setLevel(level);
+        viewModel.setSizeOfGameWindow(200, 200);
+        viewModel.startGame();
+
+        assertEquals(circles, viewModel.getCircles().getValue());
+    }
+
+    @Test
+    public void singleCircleMovesOnActionDown() {
+        // blue_ball green_ball
+        // green_ball blue_ball
+        Circle[][] circles = new Circle[1][1];
+        circles[0][0] = new Circle(50, 50, 50, R.color.blue_ball_center, false);
+        // This method is generic and not restricting the size of returned circles,
+        // so don't be fool by 3X4 at the end, it's just for readability of CirclesGenerator
+        when(circlesGenerator.generateCirclesForField3X4(anyInt(), anyInt(), anyInt()))
+                .thenReturn(circles);
+        GameLevel level = new FirstLevel();
+        viewModel.setLevel(level);
+        viewModel.setSizeOfGameWindow(200, 200);
+        viewModel.startGame();
+
+        viewModel.actionDown(60, 90);
+
+        Circle circle = viewModel.getCircles().getValue()[0][0];
+        assertEquals(60, circle.getX());
+        assertEquals(90, circle.getY());
+        assertEquals(50 - 50 / 8, circle.getRadius());
+        verify(circleObserver, times(2)).onChanged(any());
     }
 
 }
