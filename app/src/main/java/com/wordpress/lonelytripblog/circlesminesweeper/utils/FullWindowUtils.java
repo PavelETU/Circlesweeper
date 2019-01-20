3edadbb849a6a600fcdd6f44047ad9aacca658ae
pavel <pavelsuvit@gmail.com>
2019-01-20 23:12:35 +0300

    Set statusBarColor to transparent for lollipop and newer APIs (handy in multi-window mode and when fragmentDialog gets displayed)

--- app/src/main/java/com/wordpress/lonelytripblog/circlesminesweeper/utils/FullWindowUtils.java
+++ app/src/main/java/com/wordpress/lonelytripblog/circlesminesweeper/utils/FullWindowUtils.java
@@ -1,11 +1,13 @@
 package com.wordpress.lonelytripblog.circlesminesweeper.utils;
 
+import android.graphics.Color;
 import android.os.Build;
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 final public class FullWindowUtils {
 
@@ -35,5 +37,8 @@ final public class FullWindowUtils {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
